29185c7bcf81039aefa2cc786391b5b94e2dfac5
shahsarefin <shahsarefin@gmail.com>
2023-11-22 20:48:55 -0600

    2.2 - Crud-delete for admin page

--- admin/delete-admin.php
+++ admin/delete-admin.php
@@ -1,17 +1,17 @@
 <?php
 session_start();
 
-include './DB/db_connect.php'; // Database connection
+include './DB/db_connect.php'; 
 
-// Get the admin ID to delete
+
 $adminId = isset($_GET['id']) ? $_GET['id'] : null;
 
 if($adminId) {
-    // Prepare the delete query
+    
     $stmt = $pdo->prepare("DELETE FROM tbl_admin WHERE id = :id");
     $stmt->bindParam(':id', $adminId, PDO::PARAM_INT);
 
-    // Execute and check if the deletion is successful
+    
     if($stmt->execute()) {
         $_SESSION['admin_deleted'] = "Admin successfully deleted.";
     } else {
@@ -19,7 +19,7 @@ if($adminId) {
     }
 }
 
-// Redirect back to manage-admin page
+
 header('Location: manage-admin.php');
 exit();
 ?>
