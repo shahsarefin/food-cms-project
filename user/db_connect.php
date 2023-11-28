a8c31d87723c45144bbb2cd8445a831a87ed80b6
shahsarefin <shahsarefin@gmail.com>
2023-11-27 19:19:34 -0600

    style and database connection for user part

---
+++ user/db_connect.php
@@ -0,0 +1,9 @@
+<?php
+//Database connection using PDO
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=food-cms', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
